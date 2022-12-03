7d13d4580f14829896cd87d96557a48280f855b5
Alperen Aga <alperencode@gmail.com>
2022-12-04 00:18:28 +0300

    Add minor changes Update data font Disable text state to prevent changes on parsed data

--- NMEA-Project/main.py
+++ NMEA-Project/main.py
@@ -6,7 +6,6 @@ from tkinter import *
 def GUI():
     app = Tk()
     app.title("NMEA Parser")
-    # icon
     app.iconbitmap("icons/icon.ico")
     app.geometry("0x0")
     app.resizable(False, False)
@@ -24,13 +23,14 @@ def GUI():
     label = Label(frame, text="Parsed Data", font=("Calibri", 24, "bold"))
     label.pack()
 
-    text = Text(frame, width=300, height=300, font=("Arial", 12, "bold"))
+    text = Text(frame, width=300, height=300, font=("Arial", 12))
     text.pack()
 
     for key,item in parser.get_result().items():
         text.insert(END, " "*50 + f"{'-'*80}\n")
         text.insert(END, " "*70 + f"{key} : {item}\n")
 
+    text.config(state=DISABLED)
     app.mainloop()
 
 def ParseFile(sentence_file):
